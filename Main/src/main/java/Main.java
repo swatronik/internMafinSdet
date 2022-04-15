9bcb42cdba86dab6dae15e46d8253de974d47a60
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-04-15 23:17:09 +0300

    добавление возможности ввода аргументов из командной строки

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -5,26 +5,43 @@ import static java.lang.Math.sqrt;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        if (args.length == 6) {
+            for (int i = 0; i < 5; i++) {
+                if (args[i].equals("-a")) {
+                    a = Double.parseDouble(args[i + 1]);
+                }
+                if (args[i].equals("-b")) {
+                    b = Double.parseDouble(args[i + 1]);
+                }
+                if (args[i].equals("-c")) {
+                    c = Double.parseDouble(args[i + 1]);
+                }
+            }
+        } else {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите коэффицент а");
-        double a = scanner.nextDouble();
-        System.out.println("Введите коэффицент b");
-        double b = scanner.nextDouble();
-        System.out.println("Введите коэффицент c");
-        double c = scanner.nextDouble();
+            System.out.println("Enter a");
+            a = scanner.nextDouble();
+            System.out.println("Enter b");
+            b = scanner.nextDouble();
+            System.out.println("Enter c");
+            c = scanner.nextDouble();
+        }
 
         double disc = b * b - 4 * a * c;
 
         if (disc > 0) {
             double root1 = (-b - sqrt(disc)) / 2 * a;
             double root2 = (-b + sqrt(disc)) / 2 * a;
-            System.out.println(String.format("1й корень=%s 2й корень=%s", root1, root2));
+            System.out.println(String.format("1st root=%s 2nd root=%s", root1, root2));
         } else if (disc == 0) {
             double root = -b / 2 * a;
-            System.out.println(String.format("Корень=%s", root));
+            System.out.println(String.format("Root=%s", root));
         } else
-            System.out.println("Нет корней");
+            System.out.println("There are no roots");
     }
 
 }

96652fada00ad07201fc27f6a5e611928e93799b
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-04-16 13:28:32 +0300

    добавила форматирование

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -55,7 +55,7 @@ public class Main {
             double root2 = (-b + sqrt(disc)) / (2 * a);
             System.out.println(String.format("1st root=%s 2nd root=%s", root1, root2));
         } else if (disc == 0) {
-            double root = -b /( 2 * a);
+            double root = -b / (2 * a);
             System.out.println(String.format("Root=%s", root));
         } else
             System.out.println("There are no roots");

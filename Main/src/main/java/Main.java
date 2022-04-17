4f572a7100e76b8e7a3736fce2cc217d6948fb73
Ivan_Morozov <i.morozov@mafin.ru>
2022-04-18 00:46:36 +0300

    Add building fat jar file with all dependencies. Other funcs without change.

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -33,7 +33,9 @@ public class Main {
     )
     private boolean help = false;
 
-    private boolean ishelp() { return help;}
+    private boolean ishelp() {
+        return help;
+    }
 
 
     public static void main(String[] args) {
@@ -50,7 +52,7 @@ public class Main {
         }
     }
 
-    public void run() {
+    private void run() {
         double disc = getDescriminant(firstArr, secondArr, thirdArr);
         System.out.println(String.format("Discriminant = %s", disc));
         if (disc > 0) {
@@ -62,21 +64,21 @@ public class Main {
         }
     }
 
-    public static double getDescriminant(double firstArr, double secondArr, double thirdArr) {
+    private static double getDescriminant(double firstArr, double secondArr, double thirdArr) {
         return (secondArr * secondArr) - (4 * (firstArr * thirdArr));
     }
 
-    public static String getFullDecision(double firstArr, double secondArr, double thirdArr) {
+    private static String getFullDecision(double firstArr, double secondArr, double thirdArr) {
         double x1 = ((-secondArr + sqrt(getDescriminant(firstArr, secondArr, thirdArr))) / (firstArr * 2));
         double x2 = ((-secondArr - sqrt(getDescriminant(firstArr, secondArr, thirdArr))) / (firstArr * 2));
         return Arrays.toString(new double[]{x1, x2});
     }
 
-    public static double getOneDecision(double firstArr, double secondArr) {
+    private static double getOneDecision(double firstArr, double secondArr) {
         return (secondArr * -1) / (2 * firstArr);
     }
 
-    public static String getNoDecisions() {
+    private static String getNoDecisions() {
         return ("Discriminant < 0, there's no decisions");
     }
 }

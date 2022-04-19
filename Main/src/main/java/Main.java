d52ad0af658f0a11b41e9e77b0b3ef949df16097
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-04-19 12:51:23 +0300

    считывание аргументов вынесено в отдельый класс

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,49 +9,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double a = 0;
-        double b = 0;
-        double c = 0;
-        if (args.length == 6) {
-            try {
-                Option countA = new Option("a", "koeff a", true, "define a koef");
-                Option countB = new Option("b", "koeff b", true, "define b koef");
-                Option countC = new Option("c", "koeff c", true, "define c koef");
+        ParserArgumentsUtil parserArgumentsUtil = new ParserArgumentsUtil();
 
-                Options options = new Options();
+        parserArgumentsUtil.parserArgumentsUtil(args);
 
-                options.addOption(countA);
-                options.addOption(countB);
-                options.addOption(countC);
-
-                CommandLineParser parser = new DefaultParser();
-                CommandLine line = parser.parse(options, args);
-
-                if (line.hasOption("a")) {
-                    a = Double.parseDouble(line.getOptionValue("a"));
-                }
-                if (line.hasOption("b")) {
-                    b = Double.parseDouble(line.getOptionValue("b"));
-                }
-                if (line.hasOption("c")) {
-                    c = Double.parseDouble(line.getOptionValue("c"));
-                }
-            } catch (Exception e) {
-                LOGGER.error("Failed to read arguments {}", e.getMessage());
-                return;
-            }
-        } else {
-            LOGGER.error("Bad arguments");
-            return;
-        }
-        double disc = b * b - 4 * a * c;
+        double disc = parserArgumentsUtil.b * parserArgumentsUtil.b - 4 * parserArgumentsUtil.a * parserArgumentsUtil.c;
 
         if (disc > 0) {
-            double root1 = (-b - sqrt(disc)) / (2 * a);
-            double root2 = (-b + sqrt(disc)) / (2 * a);
+            double root1 = (-parserArgumentsUtil.b - sqrt(disc)) / (2 * parserArgumentsUtil.a);
+            double root2 = (-parserArgumentsUtil.b + sqrt(disc)) / (2 * parserArgumentsUtil.a);
             LOGGER.info("Result of quadratic equation is: 1st root={} 2nd root={}", root1, root2);
         } else if (disc == 0) {
-            double root = -b / (2 * a);
+            double root = -parserArgumentsUtil.b / (2 * parserArgumentsUtil.a);
             LOGGER.info("Result of quadratic equation is: root={}", root);
         } else
             LOGGER.info("There are no roots");
---
+++ Main/src/main/java/ParserArgumentsUtil.java
@@ -0,0 +1,46 @@
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ParserArgumentsUtil {
+
+    Double a = 0.0;
+    Double b = 0.0;
+    Double c = 0.0;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ParserArgumentsUtil.class);
+
+    public void parserArgumentsUtil(String[] args) {
+
+        if (args.length == 6) {
+            try {
+                Option countA = new Option("a", "koeff a", true, "define a koef");
+                Option countB = new Option("b", "koeff b", true, "define b koef");
+                Option countC = new Option("c", "koeff c", true, "define c koef");
+
+                Options options = new Options();
+
+                options.addOption(countA);
+                options.addOption(countB);
+                options.addOption(countC);
+
+                CommandLineParser parser = new DefaultParser();
+                CommandLine line = parser.parse(options, args);
+
+                if (line.hasOption("a")) {
+                    this.a = Double.parseDouble(line.getOptionValue("a"));
+                }
+                if (line.hasOption("b")) {
+                    this.b = Double.parseDouble(line.getOptionValue("b"));
+                }
+                if (line.hasOption("c")) {
+                    this.c = Double.parseDouble(line.getOptionValue("c"));
+                }
+            } catch (ParseException e) {
+                LOGGER.error("Failed to read arguments {}", e.getMessage());
+            }
+        } else {
+            LOGGER.error("Bad arguments");
+        }
+    }
+}

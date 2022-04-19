130b8495a834265d7a093961bc08bfd7d2841d2b
Vitalii_Velich <silens088@gmail.com>
2022-04-19 19:13:08 +0300

    added Scanner

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
+
 import static java.lang.Math.sqrt;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //вводим 3 числа
+        Scanner sc = new Scanner(System.in);
 
-        double a = -4;
-        double b = 28;
-        double c = -49;
+        System.out.println("Введите первый коэффициент a");
+        double a = sc.nextDouble();
+        System.out.println("Введите второй коэффициент b");
+        double b = sc.nextDouble();
+        System.out.println("Введите третий коэффициент с");
+        double c = sc.nextDouble();
 
-        //найдем дискриминант
         double discriminant = b * b - ((4 * a) * c);
         System.out.println("discriminant = " + discriminant);
 
@@ -18,14 +22,12 @@ public class Main {
             System.out.println("дискриминант равен нулю, вычислим единственный корень уравнения");
             double x = -b / (2 * a);
             System.out.println("Корень равен =  " + x);
-
         } else if (discriminant > 0) {
             System.out.println("Дискриминант положительный, найдем два действительных корня квадратного уравнения");
             double x1 = (-b + sqrt(discriminant)) / (2 * a);
             double x2 = (-b - sqrt(discriminant)) / (2 * a);
             System.out.println("Корень 1 равен " + x1);
             System.out.println("Корень 2 равен " + x2);
-
         } else
             System.out.println("Дискриминант отрицательный, действительных корней нет");
     }

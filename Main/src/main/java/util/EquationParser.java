32994219d2f0ffe8877557d24b4c2ebcfa07cc6a
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-04-22 20:35:06 +0300

    парс уравнения вынесен в отдельный класс

---
+++ Main/src/main/java/util/EquationParser.java
@@ -0,0 +1,60 @@
+package util;
+
+import exceptions.ParseArgumentsException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class EquationParser {
+    private double a = 0.0;
+    private double b = 0.0;
+    private double c = 0.0;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public EquationParser(String equation) throws ParseArgumentsException {
+        String argEquationPattern = "(((-|\\+)?[\\d]*\\.?[\\d]*)x\\^2){1}(((-|\\+)?[\\d]*\\.?[\\d]*)x)?((-|\\+)?[\\d]*\\.?[\\d]*)?(=0)";
+
+        Pattern pattern = Pattern.compile(argEquationPattern);
+
+        Matcher matcher = pattern.matcher(equation);
+
+        if (matcher.matches()) {
+            if (matcher.group(1).equals("-x^2")) {
+                this.a = -1.0;
+            } else if (matcher.group(1).equals("x^2")) {
+                this.a = 1.0;
+            } else {
+                this.a = Double.parseDouble(matcher.group(2));
+            }
+
+            if (matcher.group(4) == (null) || matcher.group(4).equals("")) {
+                this.b = 0.0;
+            } else if (matcher.group(4).equals("+x")) {
+                this.b = 1.0;
+            } else if (matcher.group(4).equals("-x")) {
+                this.b = -1.0;
+            } else {
+                this.b = Double.parseDouble(matcher.group(5));
+            }
+
+            if (matcher.group(7) == null || matcher.group(7).equals("")) {
+                this.c = 0.0;
+            } else {
+                this.c = Double.parseDouble(matcher.group(7));
+            }
+        } else {
+            throw new ParseArgumentsException(String.format("Bad arguments - this is no quadratic equation %s ", equation));
+        }
+    }
+}
--- Main/src/main/java/util/ParserArgumentsUtil.java
+++ Main/src/main/java/util/ParserArgumentsUtil.java
@@ -3,9 +3,6 @@ package util;
 import exceptions.ParseArgumentsException;
 import org.apache.commons.cli.*;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class ParserArgumentsUtil {
 
     private double a = 0.0;
@@ -55,43 +52,12 @@ public class ParserArgumentsUtil {
 
             if (type.equals("equation")) {
                 if (line.hasOption("e")) {
-
-                    String equation = "";
-                    equation = line.getOptionValue("e");
-                    String argEquationPattern = "(((-|\\+)?[0-9]*\\.?[0-9]*)x\\^2){1}(((-|\\+)?[0-9]*\\.?[0-9]*)x)?((-|\\+)?[0-9]*\\.?[0-9]*)?(=0)";
-
-                    Pattern pattern = Pattern.compile(argEquationPattern);
-
-                    Matcher matcher = pattern.matcher(equation);
-
-                    if (matcher.matches()) {
-
-                        if (matcher.group(1).equals("-x^2")) {
-                            this.a = -1.0;
-                        } else if (matcher.group(1).equals("x^2")) {
-                            this.a = 1.0;
-                        } else {
-                            this.a = Double.parseDouble(matcher.group(2));
-                        }
-
-                        if (matcher.group(4) == (null) || matcher.group(4).equals("")) {
-                            this.b = 0.0;
-                        } else if (matcher.group(4).equals("+x")) {
-                            this.b = 1.0;
-                        } else if (matcher.group(4).equals("-x")) {
-                            this.b = -1.0;
-                        } else {
-                            this.b = Double.parseDouble(matcher.group(5));
-                        }
-
-                        if (matcher.group(7) == null || matcher.group(7).equals("")) {
-                            this.c = 0.0;
-                        } else {
-                            this.c = Double.parseDouble(matcher.group(7));
-                        }
-                    } else
-                        throw new ParseArgumentsException(String.format("Bad arguments - this is no quadratic equation %s ", args));
+                    EquationParser eqargs = new EquationParser(line.getOptionValue("e"));
+                    this.a = eqargs.getA();
+                    this.b = eqargs.getB();
+                    this.c = eqargs.getC();
                 }
+
             } else if (type.equals("coefficient")) {
                 if (line.hasOption("a")) {
                     this.a = Double.parseDouble(line.getOptionValue("a"));
@@ -102,7 +68,8 @@ public class ParserArgumentsUtil {
                 if (line.hasOption("c")) {
                     this.c = Double.parseDouble(line.getOptionValue("c"));
                 }
-            } else throw new ParseArgumentsException(String.format("Bad arguments, provide correct type %s ", args));
+            } else
+                throw new ParseArgumentsException(String.format("Bad arguments, provide correct type %s ", args));
 
         } catch (NumberFormatException e) {
             throw new NumberFormatException(String.format("Invalid value of argument %s ", args));

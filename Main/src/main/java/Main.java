b206f4a5508b7155ede908302e2fccbc65928c23
Vitalii_Velich <silens088@gmail.com>
2022-04-26 00:19:32 +0300

    added slf4j q logback

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -1,33 +1,36 @@
-import java.util.Scanner;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static java.lang.Math.sqrt;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите первый коэффициент a");
-        double a = sc.nextDouble();
-        System.out.println("Введите второй коэффициент b");
-        double b = sc.nextDouble();
-        System.out.println("Введите третий коэффициент с");
-        double c = sc.nextDouble();
+        //added logger
+        final Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Just a log message.");
+        logger.debug("Message for debug level.");
 
-        double discriminant = b * b - 4 * a * c;
-        System.out.println("discriminant = " + discriminant);
+        ParserArgument parserArgument = new ParserArgument(args);
+
+        double discriminant = parserArgument.getA() * parserArgument.getB() - 4 * parserArgument.getA() * parserArgument.getC();
+        logger.info("discriminant = " + discriminant);
 
         if (discriminant == 0) {
-            System.out.println("дискриминант равен нулю, вычислим единственный корень уравнения");
-            double x = -b / (2 * a);
-            System.out.println("Корень равен =  " + x);
+
+            logger.info("discriminant = 0, get one root");
+            double x = -parserArgument.getB() / (2 * parserArgument.getA());
+            logger.info("root =  " + x);
         } else if (discriminant > 0) {
-            System.out.println("Дискриминант положительный, найдем два действительных корня квадратного уравнения");
-            double x1 = (-b + sqrt(discriminant)) / (2 * a);
-            double x2 = (-b - sqrt(discriminant)) / (2 * a);
-            System.out.println("Корень 1 равен " + x1);
-            System.out.println("Корень 2 равен " + x2);
+            logger.info("discriminant is positive, get two roots");
+            double x1 = (-parserArgument.getB() + sqrt(discriminant)) / (2 * parserArgument.getA());
+            double x2 = (-parserArgument.getB() - sqrt(discriminant)) / (2 * parserArgument.getA());
+            logger.info("root 1 = " + x1);
+            logger.info("root 2 = " + x2);
         } else
-            System.out.println("Дискриминант отрицательный, действительных корней нет");
+            logger.info("discriminant is negative, no roots");
+
     }
 }

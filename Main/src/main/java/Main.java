1ad305daae9b42bb4939bdb4d602d22c91996bcd
Vitalii_Velich <silens088@gmail.com>
2022-04-27 23:35:28 +0300

    added try-catch

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -15,17 +15,21 @@ public class Main {
         double discriminant = parserArgument.getA() * parserArgument.getB() - 4 * parserArgument.getA() * parserArgument.getC();
         logger.info("discriminant = " + discriminant);
 
-        if (discriminant == 0) {
-            logger.info("discriminant = 0, get one root");
-            double x = -parserArgument.getB() / (2 * parserArgument.getA());
-            logger.info("root = " + x);
-        } else if (discriminant > 0) {
-            logger.info("discriminant is positive, get two roots");
-            double x1 = (-parserArgument.getB() + sqrt(discriminant)) / (2 * parserArgument.getA());
-            double x2 = (-parserArgument.getB() - sqrt(discriminant)) / (2 * parserArgument.getA());
-            logger.info("root 1 = " + x1);
-            logger.info("root 2 = " + x2);
-        } else
-            logger.info("discriminant is negative, no roots");
+        try {
+            if (discriminant == 0) {
+                logger.info("discriminant = 0, get one root");
+                double x = -parserArgument.getB() / (2 * parserArgument.getA());
+                logger.info("root = " + x);
+            } else if (discriminant > 0) {
+                logger.info("discriminant is positive, get two roots");
+                double x1 = (-parserArgument.getB() + sqrt(discriminant)) / (2 * parserArgument.getA());
+                double x2 = (-parserArgument.getB() - sqrt(discriminant)) / (2 * parserArgument.getA());
+                logger.info("root 1 = " + x1);
+                logger.info("root 2 = " + x2);
+            } else
+                logger.info("discriminant is negative, no roots");
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
     }
 }

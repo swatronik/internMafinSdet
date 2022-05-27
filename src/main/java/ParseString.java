5cdf6739cd5276ca51748588be78c14fa610bcb3
s.baraev <s.baraev@mafin.ru>
2022-05-27 15:10:17 +0300

    regex

--- src/main/java/ParseString.java
+++ src/main/java/ParseString.java
@@ -3,18 +3,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParseString {
-    public static List<String> abcValues(String equation)  {
+    public static List<String> abcValues(String equation) {
         List<String> result = new LinkedList<>();
-
-            String eq = equation;
-            Pattern pattern = Pattern.compile("(((\\+|-|)((\\d+)|(\\d+\\.\\d+)))(x\\^2)((\\+|-|)((\\d+)|(\\d+\\.\\d+)))(x)((\\+|-|)((\\d+)|(\\d+\\.\\d+)))(=0))");
-            Matcher matcher = pattern.matcher(eq);
-            System.out.println(matcher.matches());
-            if(matcher.matches()){
-                result.add(matcher.group(2).replace("+", ""));
-                result.add(matcher.group(8).replace("+", ""));
-                result.add(matcher.group(14).replace("+", ""));
-            }
-            return result;
+        String eq = equation;
+        Pattern pattern = Pattern.compile("(((\\+|-|)((\\d+)|(\\d+\\.\\d+)))(x\\^2)((\\+|-|)((\\d+)|(\\d+\\.\\d+)))(x)((\\+|-|)((\\d+)|(\\d+\\.\\d+)))(=0))");
+        Matcher matcher = pattern.matcher(eq);
+        System.out.println(matcher.matches());
+        if (matcher.matches()) {
+            result.add(matcher.group(2).replace("+", ""));
+            result.add(matcher.group(8).replace("+", ""));
+            result.add(matcher.group(14).replace("+", ""));
+        }
+        return result;
     }
 }

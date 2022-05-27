bb492a12cf717ccb02c855561eef3e1da9da73ec
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-05-27 15:14:16 +0300

    переменнные сделаны final

--- Main/src/main/java/equations/Roots.java
+++ Main/src/main/java/equations/Roots.java
@@ -7,7 +7,7 @@ public class Roots {
         ONE_ROOT(1),
         TWO_ROOTS(2);
 
-        private int amount;
+        private final int amount;
 
         CountRoot(int amount) {
             this.amount = amount;
@@ -18,21 +18,24 @@ public class Roots {
         }
     }
 
-    private CountRoot countRoot;
+    private final CountRoot countRoot;
 
     public CountRoot getCountRoot() {
         return countRoot;
     }
 
-    private Double x1;
-    private Double x2;
+    private final Double x1;
+    private final Double x2;
 
     public Roots() {
+        this.x1 = null;
+        this.x2 = null;
         countRoot = CountRoot.NO_ROOTS;
     }
 
     public Roots(Double x1) {
         this.x1 = x1;
+        this.x2 = null;
         countRoot = CountRoot.ONE_ROOT;
     }
 
@@ -49,9 +52,9 @@ public class Roots {
             case NO_ROOTS:
                 return String.format("No roots");
             case ONE_ROOT:
-                return String.format("Root: x=%s", x1);
+                return String.format("Root: x= %s", x1);
             case TWO_ROOTS:
-                return String.format("Roots: x1=%s x2 =%s", x1, x2);
+                return String.format("Roots: x1= %s x2= %s", x1, x2);
             default:
                 return "";
         }

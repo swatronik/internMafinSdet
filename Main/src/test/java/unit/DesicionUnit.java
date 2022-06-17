67f97f28c2714508148333c5fdd1e930bafbca08
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-06-17 18:36:55 +0300

    правки

--- Main/src/test/java/unit/DesicionUnit.java
+++ Main/src/test/java/unit/DesicionUnit.java
@@ -24,20 +24,36 @@ public class DesicionUnit {
         Assert.assertEquals(roots.getCountRoot(),root);
     }
 
-
-    @Test
-    public void testDesicionNoRoots() {
-        Equation equation = new Equation(1, 3, 3);
-        Roots roots = EquationDecision.decision(equation);
-        Assert.assertEquals("No roots", roots.toString());
+    @DataProvider
+    public Object[][] tablesDataNoRoots() {
+        return new Object[][]{
+                {1.0,2.0,3.0,Roots.CountRoot.NO_ROOTS}
+        };
     }
 
-    @Test
-    public void testDesicionTwoRoots() {
-        Equation equation = new Equation(1, 2, -3);
+    @Test(dataProvider = "tablesDataNoRoots")
+    public void testDesicionNoRoots(double a, double b, double c, Roots.CountRoot root) {
+        Equation equation = new Equation(a, b, c);
         Roots roots = EquationDecision.decision(equation);
-        Assert.assertEquals(-3.0, roots.getX1());
-        Assert.assertEquals(1.0, roots.getX2());
+        Assert.assertNull(roots.getX1());
+        Assert.assertNull(roots.getX2());
+        Assert.assertEquals(roots.getCountRoot(),root);
+    }
+
+    @DataProvider
+    public Object[][] tablesDataTwoRoots() {
+        return new Object[][]{
+                {1.0,2.0,-3.0,-3.0,1.0,Roots.CountRoot.TWO_ROOTS}
+        };
+    }
+
+    @Test(dataProvider = "tablesDataTwoRoots")
+    public void testDesicionTwoRoots(double a, double b, double c, double x1,double x2, Roots.CountRoot root) {
+        Equation equation = new Equation(a, b, c);
+        Roots roots = EquationDecision.decision(equation);
+        Assert.assertEquals(x1, roots.getX1());
+        Assert.assertEquals(x2,roots.getX2());
+        Assert.assertEquals(roots.getCountRoot(),root);
     }
 
 }

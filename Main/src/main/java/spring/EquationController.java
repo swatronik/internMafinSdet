399f47ad410001e8563e349f35828aedbacac039
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-07-15 14:30:53 +0300

    правки

--- Main/src/main/java/spring/EquationController.java
+++ Main/src/main/java/spring/EquationController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EquationController {
 
     @RequestMapping("/")
-    public String greeting(Model model) {
-        model.addAttribute("type","квадратных");
+    public String greeting() {
         return "equation";
     }
 

2ba9dcdb1d646ce4ca641b7441ea891d75283aed
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-07-19 20:39:18 +0300

    правки

--- Main/src/main/java/spring/EquationController.java
+++ Main/src/main/java/spring/EquationController.java
@@ -1,8 +1,14 @@
 package spring;
 
+import equations.EquationDecision;
+import equations.Roots;
+import org.apache.commons.cli.ParseException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static util.Parser.parseEquation;
 
 @Controller
 public class EquationController {
@@ -12,4 +18,11 @@ public class EquationController {
         return "equation";
     }
 
+//    @PostMapping("/")
+//    public String postEquation(@RequestParam String equation)throws ParseException, NumberFormatException{
+//        Roots equationDecision=EquationDecision.decision(parseEquation(equation));
+//        System.out.println(equationDecision);
+//        return "equation";
+
 }
+

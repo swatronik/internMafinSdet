b6236b46e6486f8dd22848fa30f04032e97b7a85
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-07-27 10:31:43 +0300

    добавила обработку ошибок

--- Main/src/main/java/spring/EquationController.java
+++ Main/src/main/java/spring/EquationController.java
@@ -2,7 +2,7 @@ package spring;
 
 import equations.Equation;
 import equations.Roots;
-import exceptions.ParseArgumentsException;
+import org.apache.commons.cli.ParseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +15,22 @@ import static util.Parser.parseEquation;
 @Controller
 public class EquationController {
 
+
     @RequestMapping("/")
     public String greeting() {
         return "equation";
     }
 
     @GetMapping("/abc/{equals}")
-    public ResponseEntity<String> run(@PathVariable("equals") String equals) throws ParseArgumentsException {
-        Equation equation = parseEquation(equals);
-        Roots desicion = decision(equation);
-        return ResponseEntity.ok().body(desicion.toString());
+    public ResponseEntity<String> run(@PathVariable("equals") String equals) {
+        try {
+            Equation equation = parseEquation(equals);
+            Roots desicion = decision(equation);
+            return ResponseEntity.ok().body(desicion.toString());
+        } catch (ParseException | NumberFormatException e) {
+            return ResponseEntity.ok().body(e.getMessage());
+        }
+
     }
 }
 

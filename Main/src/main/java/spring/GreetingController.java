bc8ab33a14c98b24a61372a0b28cd6a1f29c5f8d
Vitalii_Velich <silens088@gmail.com>
2022-08-16 09:58:24 +0300

    update post method

--- Main/src/main/java/spring/GreetingController.java
+++ Main/src/main/java/spring/GreetingController.java
@@ -27,8 +27,8 @@ public class GreetingController {
         return "index";
     }
 
-    @GetMapping(value = "/getEquals/{equals}", headers = {"Accept=*/*"}) //Accept - без этого не работает
-    public ResponseEntity<String> getEquals(@PathVariable("equals") String equals) throws ExceptionMessage {
+    @PostMapping(value = "/postEqualsEquation", headers = {"Accept=*/*"})
+    public ResponseEntity<String> postEqualsEquation(@RequestBody String equals) throws ExceptionMessage {
 
         Equation equation = PatternEquation.getFullEquation(equals);
         String solution = String.valueOf(SolutionEquation.solution(equation));
@@ -37,26 +37,49 @@ public class GreetingController {
         logger.info(String.valueOf(equation));
         logger.info(solution);
 
+        JSONObject responseJSON = new JSONObject();
+        responseJSON.put("equation", equation.toString());
+
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(solution);
     }
-
-    @PostMapping(value = "/postV", headers = {"Accept=*/*"})
-    public ResponseEntity postMethod1(@RequestBody String body) {
-        //как вывести в джейсон формате ответ
-        JSONObject responseBody = new JSONObject();
-
-
-        //тут надо записать корни ответа в джейсон формат
-        responseBody
-                .put("x1", 3)
-                .put("x2", 4);
-
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(responseBody.toString());
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Кусок работоспособного кода на метод GET
+//    @GetMapping(value = "/getEquals/{equals}", headers = {"Accept=*/*"}) //Accept - без этого не работает
+//    public ResponseEntity<String> getEquals(@PathVariable("equals") String equals) throws ExceptionMessage {
+//
+//        Equation equation = PatternEquation.getFullEquation(equals);
+//        String solution = String.valueOf(SolutionEquation.solution(equation));
+//
+//        logger.info(equals);
+//        logger.info(String.valueOf(equation));
+//        logger.info(solution);
+//
+//        return ResponseEntity
+//                .ok()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(solution);
+//    }

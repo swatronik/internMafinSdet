531d26a8cea81edc78e98fc6a464ca81603508d3
Sergei Zhuravlev <s.zhuravlev@mafin.ru>
2022-10-16 08:29:01 +0300

    добавил выброс ошибки, поправил замечания

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -1,55 +1,42 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int a = 0, b = 0, c = 0;
-        double discr, x1, x2;
+    public static void main(String[] args) throws InputMismatchException {
+        double a = 0, b = 0, c = 0;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите коэффициент \"a\": ");
         try {
-            a = scanner.nextInt();
+            System.out.print("Введите коэффициент \"a\": ");
+            a = scanner.nextDouble();
             while (a == 0) {
                 System.out.print("Введите коэффициент \"a\" не равный нулю: ");
-                a = scanner.nextInt();
+                a = scanner.nextDouble();
             }
-        }
-        catch(Exception e) {
-            System.out.println("Неверный тип данных. Перезапустите программу.");
-            System.exit(1);
-        }
-
-        System.out.print("Введите коэффициент \"b\": ");
-        try {
-            b = scanner.nextInt();
-        }
-        catch(Exception e) {
-            System.out.println("Неверный тип данных. Перезапустите программу.");
-            System.exit(1);
+            System.out.print("Введите коэффициент \"b\": ");
+            b = scanner.nextDouble();
+            System.out.print("Введите коэффициент \"c\": ");
+            c = scanner.nextDouble();
+        } catch (RuntimeException e) {
+            System.out.println("Ошибка: неверный тип данных. Перезапустите программу.");
+            throw e;
         }
 
-        System.out.print("Введите коэффициент \"c\": ");
-        try {
-            c = scanner.nextInt();
-        }
-        catch(Exception e) {
-            System.out.println("Неверный тип данных. Перезапустите программу.");
-            System.exit(1);
-        }
+        System.out.println(String.format("\nВаше квадратное уравнение: (%f)x^2+(%f)x+(%f)=0\n", a, b, c));
 
-        System.out.printf("\nВаше квадратное уравнение: (%d)x^2+(%d)x+(%d)=0\n\n", a, b, c);
+        double discr = b * b - 4 * a * c;
+        double x1, x2;
 
-        discr = b * b - 4 * a * c;
-
-        if(discr > 0) {
-            x1 = (b*(-1) + Math.sqrt(discr))/(2*a);
-            x2 = (b*(-1) - Math.sqrt(discr))/(2*a);
-            System.out.printf("Решение: x1 = %f, x2 = %f.", x1, x2);
+        if (discr > 0) {
+            x1 = (b * (-1) + Math.sqrt(discr)) / (2 * a);
+            x2 = (b * (-1) - Math.sqrt(discr)) / (2 * a);
+            System.out.print(String.format("Решение: x1 = %f, x2 = %f.", x1, x2));
         } else if (discr == 0) {
-            x1 = (b*(-1))/(2*a);
-            System.out.printf("Решение: x = %f", x1);
-        } else
-            System.out.println("Уравнение не имеет корней.");
+            x1 = (b * (-1)) / (2 * a);
+            System.out.print(String.format("Решение: x = %f", x1));
+        } else {
+            System.out.print("Уравнение не имеет корней.");
+        }
     }
 }

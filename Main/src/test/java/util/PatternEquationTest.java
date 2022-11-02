1910c415fbd9bebb7b1546857e99e9710ae97627
Vitalii_Velich <silens088@gmail.com>
2022-11-02 22:55:39 +0300

    added Junit parametrised test negative

--- Main/src/test/java/util/PatternEquationTest.java
+++ Main/src/test/java/util/PatternEquationTest.java
@@ -7,34 +7,46 @@ import patternRegexp.PatternRegexp;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PatternEquationTest {
 
     @ParameterizedTest(name = "#{index} - Run test with equation = {0}")
-    @MethodSource("equationExample")
-    void EquationValidatorTest(String equation) {
+    @MethodSource("equationExamplePositive")
+    void EquationValidatorPositiveTest(String equation) {
         boolean result = equation.matches(PatternRegexp.patternEquation);
         assertTrue(result);
     }
-    static Stream<String> equationExample() {
+    static Stream<String> equationExamplePositive() {
         return Stream.of(
                 "4x^2+0x+1=0",
                 "-4x^2+2x+1=0",
                 "4x^2+2x+1=0",
-                "ololo",
-                "1111111",
-                "111x^2+222x+335g3=0",
                 "-4x^2-2x-1=0",
                 "44x^2+2x+1=0",
                 "111x^2-222x-333=0",
                 "111x^2+222x+333=0",
                 "5675.3243x^2-8x+12=0",
                 "5675.3243x^2-88888.8888x+12=0",
-                "5675.3243x^2-88888.8888x+999.9999=0",
-                "bad test3"
+                "5675.3243x^2-88888.8888x+999.9999=0"
         );
     }
+
+    @ParameterizedTest(name = "#{index} - Run test with equation = {0}")
+    @MethodSource("equationExampleNegative")
+    void EquationValidatorNegativeTest(String equation) {
+        boolean result = equation.matches(PatternRegexp.patternEquation);
+        assertFalse(result);
+    }
+    static Stream<String> equationExampleNegative() {
+        return Stream.of(
+                "111x^2+222x+335g3=0",
+                "bad test3",
+                "11y2+2x+1=0"
+        );
+    }
+
 
     @Test
     void getFullEquation() {

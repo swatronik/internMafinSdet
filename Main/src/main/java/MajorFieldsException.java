4ca961550ed29bf48f2743ce221ff9eaf8a96ff6
Polyakov <d.polyakov@infomaximum.com>
2023-02-26 09:54:58 +0300

    revert error commit

--- Main/src/main/java/MajorFieldsException.java
+++
@@ -1,7 +0,0 @@
-public class MajorFieldsException extends RuntimeException {
-
-    public MajorFieldsException(String field) {
-        super(String.format("Обязательное поле %s не заполнено", field));
-    }
-
-}

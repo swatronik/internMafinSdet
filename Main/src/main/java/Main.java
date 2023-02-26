f834792e94f1ac42a9b2e6f5bba23c335b5553b2
Polyakov <d.polyakov@infomaximum.com>
2023-02-26 09:55:32 +0300

    revert error commit

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -1,16 +1,5 @@
 public class Main {
 
     public static void main(String[] args) {
-        Ticket ticket = new Ticket()
-                .withFirstName("Evgeny")
-                .withLastName("Malgin")
-                .withCountry("Russia")
-                .withGender("man")
-                .withDateOfBirth("12.08.1995")
-                .withMiddleName("123");
-
-        //Ticket ticket = new Ticket("Evgeny","Malgin","man","12.08.1995",null,null);
-        //Ticket ticket2 = new Ticket("Evgeny","Malgin","man","12.08.1995");
-        ticket.registration();
     }
 }

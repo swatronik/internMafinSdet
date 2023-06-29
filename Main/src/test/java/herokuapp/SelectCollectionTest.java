a16f4c0e9612bca5278b713a38e56e55bd49235d
Vitalii_Velich <silens088@gmail.com>
2023-06-29 19:37:43 +0300

    update test: SelectCollectionTest

--- Main/src/test/java/herokuapp/SelectCollectionTest.java
+++ Main/src/test/java/herokuapp/SelectCollectionTest.java
@@ -10,24 +10,27 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelectCollectionTest {
 
     private final static String URL = "https://the-internet.herokuapp.com/";
+    private final static String DragDropElementLink = "div#content a[href*=\"/drag_and_drop\"]";
 
     @Test
     public void openSiteAndParseLinks() {
         Selenide.open(URL);
         ElementsCollection collect = $$("div#content a[href]");
         List<String> listWithLinks = new ArrayList<>();
-        for (SelenideElement el : collect) {
-            listWithLinks.add(el.getAttribute("href"));
+        for (SelenideElement element : collect) {
+            listWithLinks.add(element.getAttribute("href"));
         }
-        String drag_and_drop_URL = listWithLinks.get(9);
-        open(drag_and_drop_URL);
 
+        $(DragDropElementLink).shouldHave(text("Drag and Drop")).click();
+
+        String dragDropLink = listWithLinks.get(9);
         String currentLink = WebDriverRunner.getWebDriver().getCurrentUrl();
-        Assert.assertEquals(currentLink, drag_and_drop_URL);
+        Assert.assertEquals(currentLink, dragDropLink);
     }
 }
